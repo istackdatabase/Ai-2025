74f68d02e351f25ea0db37e4a3deef1326c6d073
istackdatabase <istackdatabase@gmail.com>
2025-03-23 13:42:58 +0530

    Add files via upload
    playground app

---
+++ playground.py
@@ -0,0 +1,39 @@
+from phi.agent import Agent
+from phi.model.openai import OpenAIChat
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+import openai
+import phi
+import os
+from dotenv import load_dotenv
+from phi.playground import Playground, serve_playground_app
+
+phi.api=os.getenv("PHI_API_KEY")
+
+##web agent
+web_search_agent = Agent(
+    name="Web Agent",
+    #model=OpenAIChat(id="gpt-4o"),
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tool_calls=True,
+    markdown=True,
+)
+#web_search_agent.print_response("Whats happening in France?", stream=True)
+
+##finance agent
+finance_agent = Agent(
+    name="Finance Agent",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True, company_news=True)],
+    instructions=["Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+app = Playground(agents=[finance_agent, web_search_agent]).get_app()
+
+if __name__ == "__main__":
+    serve_playground_app("playground:app", reload=True)
